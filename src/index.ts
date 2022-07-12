ae800ac0fa9de6ecf5e28e848a7f3805056c83cd
Rafael Vilomar <rafael.vilomar@mqaamericas.com>
2022-07-11 23:46:45 -0400

    feat: add Jest library #1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import express, { Express } from "express";
 import { Server as WebSocketServer } from "socket.io";
 import http from "http";
 
+import app from "./App";
 import envConfig from "./utils/envConfig";
 import socketConfig from "./utils/socketConfig";
 
-const app: Express = express();
 const server = http.createServer(app);
 const io = new WebSocketServer(server, { cors: { origin: "*" } });
 
@@ -14,3 +13,5 @@ server.listen(envConfig.api.port, () => {
   // eslint-disable-next-line no-console
   console.info(`Started :) | Listening on: ${envConfig.api.port}`);
 });
+
+export default server;
