f999bce6c169f371623b3c9feaed783b361c9070
Rafael Vilomar <rafavilomar@gmail.com>
2024-05-12 23:05:27 -0400

    chore: change Procfile entry point

--- dist/src/ModelsMongo/UserModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = __importDefault(require("mongoose"));
-const UserSchema_1 = __importDefault(require("../Schemas/UserSchema"));
-const UserModel = mongoose_1.default.model("User", UserSchema_1.default);
-exports.default = UserModel;
