56243bd6492fe5168553367390638c64e19813e9
Rafael Vilomar <rafavilomar@gmail.com>
2024-05-19 21:29:57 -0400

    feat: add live testing endpoint

--- src/App.ts
+++ src/App.ts
@@ -3,4 +3,8 @@ import express, { Express } from "express";
 const app: Express = express();
 app.use(express.json());
 
+app.get("/live", (req, res) => {
+    res.send("I'm here!")
+})
+
 export default app;
